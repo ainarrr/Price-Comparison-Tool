b07db3d93b232f64bcbf7944b7daa9e534fefc0b
Aina <159388318+ainarrr@users.noreply.github.com>
2025-06-26 10:17:24 +1200

    working pandas title

--- panda_fix.py
+++ panda_fix.py
@@ -109,13 +109,10 @@ measurement_ans = ('weight', 'w', 'volume', 'v', 'quantity', 'q')
 all_items = []
 all_amounts = []  # grams or millilitres or quantity
 all_costs = []
+converted_amounts = []
 
-# Create DataFrame without converted column yet
-price_tool_dict = {
-    "Item": all_items,
-    "Amount (g/ml)": all_amounts,
-    "Cost": all_costs
-}
+
+
 
 print("Price Comparison Tool")
 
@@ -133,11 +130,11 @@ item_type = string_check("Are your items measured in weight, volume, or quantity
                          valid_answers=measurement_ans,
                          num_letters=1)
 
+
 # Start item entry loop
 while True:
     item = not_blank("Item Name: ")
 
-# Obligates user to enter minimum of 2 items/entries
     if item.lower() == "xxx":
         if len(all_items) < MIN_ITEMS:
             print("Oops, you need at least two items before exiting.\n")
@@ -145,41 +142,56 @@ while True:
         else:
             break
 
-    # Unit details
+    # Determine unit names
     if item_type in ['w', 'weight']:
         unit_name = "g"
         converted_unit = "kg"
+        amount_label = "Amount (g)"
     elif item_type in ['v', 'volume']:
         unit_name = "ml"
         converted_unit = "L"
+        amount_label = "Amount (ml)"
     else:
-
         unit_name = "qty"
         converted_unit = "qty"
+        amount_label = "Quantity"
 
-    # Allows users input to be converted so it can be calculated later on as $/kg or $/litre or $/quantity
-    if item_type == "quantity":
+    # Get user amount when quantity
+    if item_type in ['q', 'quantity']:
         amount = num_check("Enter how many items (e.g. eggs): ", "integer")
     else:
         amount = num_check(f"Enter the item amount (in {unit_name}): ")
 
+    # Convert amount to standard unit
+    if item_type in ['w', 'weight', 'v', 'volume']:
+        converted_amount = amount / 1000
+    else:
+        converted_amount = amount
 
-    # Asking user for the item's cost
+    # Get the item cost
     cost = num_check("Enter the item cost $", "float")
 
-    # Append values to lists
+    # Append the data
     all_items.append(item)
     all_amounts.append(amount)
     all_costs.append(cost)
+    converted_amounts.append(converted_amount)
+
 
 go_over_budget = string_check("Do you want to see recommendation even if they're over your budget? ", ('yes', 'no'))
 
-
+# Create DataFrame without converted column yet
+price_tool_dict = {
+    "Item": all_items,
+    amount_label: all_amounts,
+    "Cost": all_costs
+}
 # create dataframe / table from dictionary
 price_tool_frame = pandas.DataFrame(price_tool_dict)
 
 # Automatically convert to kg/L/qty based on user unit
-price_tool_frame[f"Amount ({converted_unit})"] = price_tool_frame[f"Amount ({unit_name})"] / 1000
+price_tool_frame[f"Amount ({converted_unit})"] = converted_amounts
+
 
 # Calculate Unit Price
 price_tool_frame["Unit Price"] = (price_tool_frame["Cost"] / price_tool_frame[f"Amount ({converted_unit})"]).round(2)
